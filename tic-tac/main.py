0e1d3986d3075968911ebf6d09afe208f4bf68a3
Gezim Haziri <gezim@ec.is>
2018-01-13 20:25:25 +0000

    main

---
+++ tic-tac/main.py
@@ -0,0 +1,19 @@
+"""Module implements Machine Learning algorithm for a Tic-Tac-Toe game."""
+import sys
+
+
+
+class Game(object):
+    """docstring for Game."""
+
+    def __init__(self, arg):
+        super(Game, self).__init__()
+        self.arg = arg
+
+
+def main():
+    """Run the program in the terminal."""
+    print("Enter something:")
+
+    if __name__ == '__main__':
+        main()
