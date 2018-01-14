2db2feb33544721adecd739bf9eb118f7de2fb0e
Gezim Haziri <gezim@ec.is>
2018-01-14 09:48:35 -0800

    mongodb database - with basic functions

--- tic-tac/main.py
+++ tic-tac/main.py
@@ -6,6 +6,7 @@ from time import sleep
 
 # 3rd party libraries
 import numpy
+from pymongo import MongoClient
 
 
 class Game(object):
@@ -31,18 +32,44 @@ class Game(object):
             print(line)
 
     def check_if_winning_ticket(self):
-        """TODO
-        Checks if the 3x3 matrix has a winning row, column or diagonal."""
+        """TODO: Checks if the 3x3 matrix has a winning row, column or
+        diagonal."""
         pass
 
     def rules(self):
-        """TODO
-        Implement the rules of the game."""
+        """TODO: Implement the rules of the game."""
         pass
+
+
+class Database(object):
+    """Connects to mongodb database."""
+    def __init__(self, database):
+        super(Database, self).__init__()
+        self.connect(database)
+
+    def connect(self, database):
+        user = "admin"
+        password = "9QadJmq4yNF9aqOB"
+        server = "SG-tictactoe-12232.servers.mongodirector.com"
+        port = "27017"
+        uri = "mongodb://%s:%s@%s:%s/admin" % (user, password, server, port)
+        client = MongoClient(uri)
+        self.db = client[database]
+
+    def add_data(self, collection, data):
+        # data is a dictionary or a list of dictionaries
+        if isinstance(data, list):
+            return self.db[collection].insert_one(data).inserted_id
+        else:
+            return self.db[collection].insert_many(data).inserted_ids
+
+    def get_first(self, collection):
+        return self.db[collection].find_one()
 
 
 def main():
     """Run the program in the terminal."""
+
 
     user_input = input("How many games to print?: ")
     rounds = int(user_input) # TODO: Check if input is a number
